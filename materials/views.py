697b25c47df3150b0c34fcb47822248d7220e8b5
Konstantin Maurin <artsjesus7@gmail.com>
2024-11-19 22:06:27 +0300

    add get_permissions

--- materials/views.py
+++ materials/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, generics
 from materials.models import Course, Lesson
 from materials.serializers import CourseSerializer, LessonSerializer, CourseDetailSerializer
+from users.permissions import IsModer, IsOwner
+from rest_framework.permissions import IsAuthenticated
 
 
 class CourseViewSet(viewsets.ModelViewSet):
@@ -13,9 +15,26 @@ class CourseViewSet(viewsets.ModelViewSet):
             return CourseDetailSerializer
         return CourseSerializer
 
+    def get_permissions(self):
+        if self.action in ["create", "destroy"]:
+            self.permission_classes = (~IsModer,)
+        elif self.action in ["update", "retrieve"]:
+            self.permission_classes = (IsModer | IsOwner, )
+        elif self.action == "destroy":
+            self.permission_classes = (~IsModer | IsOwner,)
+        return super.get_permissions()
+
 
 class LessonCreateAPIView(generics.CreateAPIView):
     serializer_class = LessonSerializer
+    permission_classes = (~IsModer, IsAuthenticated)
+
+    def perform_create(self, serializer):
+        """ Этот метод срабатывает, когда пользователь создает новый урок через API."""
+
+        course = serializer.save()
+        course.owner = self.request.user
+        course.save()
 
 
 class LessonListAPIView(generics.ListAPIView):
@@ -26,12 +45,15 @@ class LessonListAPIView(generics.ListAPIView):
 class LessonRetrieveAPIView(generics.RetrieveAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = (IsAuthenticated, IsModer | IsOwner)
 
 
 class LessonUpdateAPIView(generics.UpdateAPIView):
     serializer_class = LessonSerializer
     queryset = Lesson.objects.all()
+    permission_classes = (IsAuthenticated, IsModer | IsOwner)
 
 
 class LessonDestroyAPIView(generics.DestroyAPIView):
     queryset = Lesson.objects.all()
+    permission_classes = (IsAuthenticated, IsOwner | ~IsModer)

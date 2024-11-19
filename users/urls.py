15aa2b3b4d0e3464c2ffed2240772cab1d51bf40
Konstantin Maurin <artsjesus7@gmail.com>
2024-11-19 22:05:35 +0300

    create urls user

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
-
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 from users.apps import UsersConfig
-from users.views import PaymentListView
+from users.views import PaymentListView, UserCreateAPIView
+from rest_framework.permissions import AllowAny
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path("payments/", PaymentListView.as_view(), name="payment_list"),
+    path("register/", UserCreateAPIView.as_view(), name="register"),
+    path("login/", TokenObtainPairView.as_view(permission_classes=(AllowAny,)), name="login",),
+    path("token/refresh/", TokenRefreshView.as_view(permission_classes=(AllowAny,)), name="token_refresh",
+    ),
 ]
